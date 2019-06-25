51a7a847ebef6f7462215dd4ec4b12667f4d2751
nabacg <nabacg@gmail.com>
2019-06-25 14:46:10 +0100

    notebook for colab training

---
+++ language-model/seq2seqdataprocessing.py
@@ -0,0 +1,122 @@
+import datasets
+import os
+import re
+from collections import Counter
+import tensorflow as tf
+
+
+# Converts the unicode file to ascii
+def unicode_to_ascii(s):
+    return ''.join(c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn')
+
+
+def preprocess_sentence(w):
+#     w = unicode_to_ascii(w.lower().strip())
+    w = w.lower().strip()
+    
+    # creating a space between a word and the punctuation following it
+    # eg: "he is a boy." => "he is a boy ." 
+    # Reference:- https://stackoverflow.com/questions/3645931/python-padding-punctuation-with-white-spaces-keeping-punctuation
+    w = re.sub(r"([?.!,¿])", r" \1 ", w)
+    w = re.sub(r'[" "]+', " ", w)
+    
+    # replacing everything with space except (a-z, A-Z, ".", "?", "!", ",")
+#     w = re.sub(r"[^a-zA-Z?.!,¿]+", " ", w)
+    w = re.sub(r"[^a-zA-Z]+", " ", w)
+    w = w.rstrip().strip()
+    
+    # adding a start and an end token to the sentence
+    # so that the model know when to start and stop predicting.
+    w = '<start> ' + w + ' <end>'
+    return w
+
+# This class creates a word -> index mapping (e.g,. "dad" -> 5) and vice-versa 
+# (e.g., 5 -> "dad") for each language,
+
+
+UNK_WORD_INDEX = 1
+def create_index(phrases, vocab_size = 15000):   # why 15k ? because that's what they used in https://arxiv.org/pdf/1406.1078.pdf
+    word2idx = {}
+#     vocab_size = 15000 
+    idx2word = {}
+    vocab = set()
+    # https://docs.python.org/3/library/collections.html#collections.Counter
+    wordcount = Counter([p for s in phrases for p in s.split(' ')])
+
+    vocab = sorted([w for (w, c) in wordcount.most_common(vocab_size)])
+    
+    word2idx['<pad>'] = 0
+    word2idx['<unk>'] = UNK_WORD_INDEX
+    for index, word in enumerate(vocab):
+        word2idx[word] = index + 2
+    
+    for word, index in word2idx.items():
+        idx2word[index] = word
+        
+    return word2idx, idx2word, vocab
+  
+def word_to_idx(lookup, word):
+  return lookup.get(word, UNK_WORD_INDEX)
+
+def idx_to_word(lookup, idx): 
+  return lookup.get(idx, '<unk>')
+
+
+def grouped(iterable, n):
+    "s -> (s0,s1,s2,...sn-1), (sn,sn+1,sn+2,...s2n-1), (s2n,s2n+1,s2n+2,...s3n-1), ..."
+    return zip(*[iter(iterable)]*n)
+
+def readTwitterData():
+  data_file =  open('data/chat_corpus/twitter_en.txt')
+  return [(preprocess_sentence(q), preprocess_sentence(a)) for (q,a) in grouped(data_file, 2)]
+ 
+def create_dataset(dataset_name, max_sentence_length):
+    dataset_path = 'data/{}'.format(dataset_name)
+
+    if dataset_name == "cornell":
+        data = datasets.readCornellData(dataset_path, max_len=max_sentence_length)
+    elif dataset_name == "opensubs":
+        data = datasets.readOpensubsData(dataset_path, max_len=max_sentence_length)
+    elif dataset_name == 'twitter':
+        data = readTwitterData()
+    else:
+        raise ValueError("Unrecognized dataset: {!r}".format(dataset_name))
+    
+    return data
+
+def max_length(tensor):
+    return max(len(t) for t in tensor)
+
+# def preprocess_sentence(s):
+#     return '<start> ' + s + ' <end>'
+
+
+def load_dataset(dataset_name = 'cornell', max_sentence_length= 10):
+    # creating cleaned input, output pairs
+    pairs = [(preprocess_sentence(a), preprocess_sentence(b)) for (a,b) in create_dataset(dataset_name, max_sentence_length)]
+
+    # index language using the class defined above    
+    word2idx, idx2word, vocab = create_index([p for ps in pairs for p in ps])
+    # Vectorize the input and target languages
+    
+    # question sentences
+    input_tensor = [[word_to_idx(word2idx, w) for w in qs.split(' ')] for qs, a in pairs]
+    
+    # answer sentences
+    target_tensor = [[word_to_idx(word2idx, w) for w in a.split(' ')] for q, a in pairs]
+    
+    # Calculate max_length of input and output tensor
+    # Here, we'll set those to the longest sentence in the dataset
+    max_length_inp, max_length_tar = max_length(input_tensor), max_length(target_tensor)
+    
+    # Padding the input and output tensor to the maximum length
+    input_tensor = tf.keras.preprocessing.sequence.pad_sequences(input_tensor, 
+                                                                 maxlen=max_length_inp,
+                                                                 padding='post')
+    
+    target_tensor = tf.keras.preprocessing.sequence.pad_sequences(target_tensor, 
+                                                                  maxlen=max_length_tar, 
+                                                                  padding='post')
+    
+    return input_tensor, target_tensor, (word2idx, idx2word, vocab), max_length_inp, max_length_tar
